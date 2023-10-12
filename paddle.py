4611299e35987b0ac599e6d8b413001064a92a7c
Mayank Tripathi <tripathimayank216@gmail.com>
2023-10-12 09:59:12 +0530

    Initial Commit

---
+++ paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+
+    def __init__(self, pos):
+        super().__init__()
+        self.color("white")
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.speed("fastest")
+        self.goto(pos)
+
+    def move_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def move_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
